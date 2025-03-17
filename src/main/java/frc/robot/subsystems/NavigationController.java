f79478e6e8a0600071a4681fbbc27085cbefa541
Ray <rayang970526@gmail.com>
2025-03-17 13:34:45 +0800

    nig

--- src/main/java/frc/robot/subsystems/NavigationController.java
+++ src/main/java/frc/robot/subsystems/NavigationController.java
@@ -310,7 +310,7 @@ public class NavigationController extends SubsystemBase {
             return;
         } else if (destination == DestinationState.PATHFINDING_TO_LEFT_OF_TAG) {
             // Navigate to the left side of the closest tag
-            boolean success = navigateToClosestTag(0.6, -0.164, null);  // 1.0m to the left
+            boolean success = navigateToClosestTag(0.55, -0.164, null);  // 1.0m to the left
             if (success) {
                 SmartDashboard.putString("Navigation/Status", "Navigating to left of closest tag");
             } else {
@@ -319,7 +319,7 @@ public class NavigationController extends SubsystemBase {
             return;
         } else if (destination == DestinationState.PATHFINDING_TO_RIGHT_OF_TAG) {
             // Navigate to the right side of the closest tag
-            boolean success = navigateToClosestTag(0.6, 0.164, null);  // -1.0m to the left (= right)
+            boolean success = navigateToClosestTag(0.55, 0.164, null);  // -1.0m to the left (= right)
             if (success) {
                 SmartDashboard.putString("Navigation/Status", "Navigating to right of closest tag");
             } else {
